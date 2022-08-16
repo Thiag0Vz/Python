3ed6f4fa137b4a13b0abf6ab9c8410d9e49505b3
Thiag0Vz <111368080+Thiag0Vz@users.noreply.github.com>
2022-08-16 11:19:28 -0300

    Add files via upload

---
+++ Conversão de base numérica.py
@@ -0,0 +1,12 @@
+print ('Digite um número')
+n = int(input())
+
+print ('Deseja converter esse número para qual base? ')
+print ('1. Binário')
+print ('2. Octal')
+print ('3. Hexadecimal')
+
+r = int(input())
+
+if r == 1:
+    
