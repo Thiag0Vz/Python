a6c4b28a7f8b62219cfafa1c6173e28824a17cb5
Thiag0Vz <111368080+Thiag0Vz@users.noreply.github.com>
2022-08-16 12:04:34 -0300

    Delete Cadastro e verificação de nota.py

--- Cadastro e verificação de nota.py
+++
@@ -1,7 +0,0 @@
-n = -1
-while (n < 0) or (n > 10):
-    print ('Digite uma nota')
-    n = float(input())
-    if (n < 0) or (n > 10):
-        print ('Valor inválido')
-print ('O valor digitado foi {}'.format(n))
