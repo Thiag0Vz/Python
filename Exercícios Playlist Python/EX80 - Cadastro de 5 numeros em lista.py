4a2daf4a9b8e706926bb56043d7c816f3ce98a71
Thiago Vaz <thiagov.projeto@gmail.com>
2022-08-23 17:33:19 -0300

    Add files via upload

--- Exercícios Playlist Python/EX80 - Cadastro de 5 numeros em lista.py
+++ Exercícios Playlist Python/EX80 - Cadastro de 5 numeros em lista.py
@@ -1,15 +1,23 @@
-c = 0
 ls = []
-nm = 0
-count = 0
+c = 0
+
+
 while (c < 5):
     print ('Digite um número: ')
-    n = int(input())
-    ls.append(n)
-    c += 1 
-    count += 1
+    n =  int(input())
+    c += 1
     
-    if (count == 1):
-        
+    if (c == 1) or (n > ls[-1]):
+        ls.append(n)
+    else:
+         pos = 0 # Esse pos é para verificar conforme a posição da lista
+         while pos < len(ls): #Enquanto a posição for menor que o número da lista - ou seja, vai andar até o final - vai verificar o seguinte:
+            if (n <= ls[pos]): #Se n for menor igual ao número da lista na posição pos, ele vai inserir nessa posição e parar o programa. 
+                ls.insert(pos,n)
+                break
+            print (ls) #Coloquei esse print pra facilitar a visualização do loop. Quando ele ocorre, significa que o número inserido foi maior, ou seja, não corresponde ao if
 
-print (ls)
+            pos += 1 #Se não for, vai acrescentar 1 e repetir o loop desde o início
+
+
+print (f'Final {ls}')
